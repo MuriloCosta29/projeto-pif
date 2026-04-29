591732be80c54a8d0697639bd1f0c8a1f3e25718
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-04-29 15:20:02 -0300

    feat: criar janela inicial do jogo closes #3

--- src/main.c
+++ src/main.c
@@ -1,2 +1,16 @@
-#include <stdio.h>
-int main(void) { printf("Hello, World!"); }
+#include <raylib.h>
+
+int main() {
+  InitWindow(800, 600, "Stan James"); // Initialize window and OpenGL context
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();            // Setup canvas (framebuffer) to start drawing
+    ClearBackground(RAYWHITE); // Set background color (framebuffer clear color)
+    DrawText("Stan James", 300, 280, 40,
+             BLACK); // Draw text (using default font)
+    EndDrawing();    // End canvas drawing and swap buffers (double buffering)
+  }
+
+  CloseWindow(); // Close window and unload OpenGL context
+  return 0;
+}
